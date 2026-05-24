970089e34b98aad4457845199fc51b9ec35c73b4
srinathpathak5-del <srinathpathak5@gmail.com>
2026-05-24 11:37:24 +0530

    Remove MobileEntry temporarily

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -2,15 +2,14 @@
 import Ticker from './layout/Ticker'
 import Navbar from './layout/Navbar'
 import Footer from './layout/Footer'
-import MobileEntry from './MobileEntry'
 
 export default function ClientWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <MobileEntry>
+    <>
       <Ticker />
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </MobileEntry>
+    </>
   )
 }
