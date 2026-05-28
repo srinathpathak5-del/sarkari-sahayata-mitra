4190c38b235d52c6b625d61f58d858cafd5d6366
srinathpathak5-del <srinathpathak5@gmail.com>
2026-05-28 23:35:56 +0530

    Added Google Ads conversion tag

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,11 +1,12 @@
 // File location: components/GoogleAnalytics.tsx
-// Google Analytics 4 tracking for sahayatamitra.com
-// Measurement ID: G-JTLRHX2HT9
+// Loads BOTH Google Analytics (G-JTLRHX2HT9) AND Google Ads (AW-18195168856) tags
+// This enables visitor tracking AND ads conversion tracking
 
 'use client'
 import Script from 'next/script'
 
-const GA_MEASUREMENT_ID = 'G-JTLRHX2HT9'
+const GA_MEASUREMENT_ID = 'G-JTLRHX2HT9'      // Google Analytics
+const GOOGLE_ADS_ID = 'AW-18195168856'         // Google Ads conversion tracking
 
 export default function GoogleAnalytics() {
   return (
@@ -14,7 +15,7 @@ export default function GoogleAnalytics() {
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-tags" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -22,6 +23,7 @@ export default function GoogleAnalytics() {
           gtag('config', '${GA_MEASUREMENT_ID}', {
             page_path: window.location.pathname,
           });
+          gtag('config', '${GOOGLE_ADS_ID}');
         `}
       </Script>
     </>
